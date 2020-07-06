e0c02dee3edb784a45e607944825fa743becf909
qianyi.zhang <qianyi@com>
2020-07-06 13:43:48 +0800

    two problem should solve
    1: the value that get_rdtsc() return is long long type,but
       cdll.LoadLibrary is return int, so we can not get the
       correct value
    
    2: function of sendto is needed bytes,so i need to chage the
       correct format

--- client.py
+++ client.py
@@ -1,9 +1,26 @@
+import socket
+import numpy as np
 from ctypes import *
-import sys
+from ctypes import c_longlong as ll
+import time
+BUFSIZE = 1024
 try:  
     lib = cdll.LoadLibrary('./rdtsc.so')  
 except:  
     print("error for load lib")
     exit
-target = lib.get_rdtsc()
-print ("0x%x"%target)
+client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+ip_port = ('127.0.0.1', 9999)
+data = []
+for i in range(1,100):
+    data.append(i)
+while True:
+    lib.get_rdtsc.restype = c_longlong
+    target = lib.get_rdtsc()
+    print(target)
+    data.append(target)
+    msg = str(data)
+    client.sendto(msg.encode(),ip_port)
+    data.pop()
+    time.sleep(1)
+client.close()
--- rdtsc.c
+++ rdtsc.c
@@ -1,10 +1,11 @@
-typedef unsigned long long __u64;
-typedef unsigned long __u32;
-int get_rdtsc(){
+typedef long long __u64;
+typedef long __u32;
+__u64 get_rdtsc(){
     __u32 hi,lo;
     __asm__ __volatile__ 
     (
         "rdtsc" : "=a" (lo),"=d"(hi)
     );
+
     return (__u64) hi<<32|lo;
 }
