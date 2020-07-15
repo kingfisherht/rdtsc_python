d526c8ece6807f179bfb3868b0aff0fc95c1e020
qianyi <qianyi.zhang@intel.com>
2020-07-15 14:15:31 +0800

    fix error for run

--- time_stamp.py
+++ time_stamp.py
@@ -1,18 +1,20 @@
-import sched
 import time
 from datetime import datetime
-def timetostamp(stamp):
+def stamptotime(stamp):
     timeStamp = float(stamp/1000) 
     timeArray = time.localtime(timeStamp) 
-    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S.%f", timeArray)
     print(otherStyleTime)
 
 def timetoStamp(timestr):
     datetime_obj = datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S.%f")
     obj_stamp = int(time.mktime(datetime_obj.timetuple()) * 1000.0 + datetime_obj.microsecond / 1000.0)
     print(obj_stamp)
+    return obj_stamp
+
 if __name__ == "__main__":
     data = datetime.now()
     print("data:",data)
-    timetoStamp(data)
+    stampd = timetoStamp(str(data))
+    stamptotime(stampd)
 
