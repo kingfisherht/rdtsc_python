deb55fe37c7ee93c1fad5eb824519e592e8ed996
qianyi <qianyi.zhang@intel.com>
2020-07-16 09:35:08 +0800

    this meold is not accurate
    becaus next time is rely on our function of printTime

--- timer-ms.py
+++ timer-ms.py
@@ -3,30 +3,30 @@ import time
 from datetime import datetime
 schedule = sched.scheduler(time.time, time.sleep)
 
-def timetostamp(stamp):
-    timeStamp = float(stamp/1000000) 
-    timeArray = time.localtime(timeStamp) 
-    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-    #print(otherStyleTime)
-
 def printTime(inc,start_ms,start_s):
-    expect = start_ms + 4
-    #timetostamp(start)
+    expect_ms = start_ms + 4000
+    expect_s = start_s
+    if(expect_ms > 1000000):
+        expect_ms -= 1000000
+        expect_s += 1
     data_now = datetime.now()
     data_s = int(time.mktime(data_now.timetuple()))
     data_ms = data_now.microsecond
-    s_diff = data_s - start_s
-    ms_diff = data_ms - expect
+    s_diff = data_s - expect_s
+    ms_diff = data_ms - expect_ms
     print("data_ms:",data_ms)
-    print("expect:",expect)
+    print("expect_ms:",expect_ms)
+    print("data_s:",data_s)
+    print("expect_s:",expect_s)
     print("s_diff:",s_diff)
     print("ms_diff:",ms_diff)
-    print(" ")
-    schedule.enter(inc, 0, printTime, (inc,expect,start_s))
+    print("latency: ",s_diff*1000000 + ms_diff)
+    print("")
+    schedule.enter(inc, 0, printTime, (inc,expect_ms,expect_s))
 
 if __name__ == "__main__":
     start_time = datetime.now()
     start_ms = start_time.microsecond
     start_s = int(time.mktime(start_time.timetuple()))
-    schedule.enter(0, 0, printTime, (0.04,start_ms,start_s))
+    schedule.enter(0, 0, printTime, (0.004,start_ms,start_s))
     schedule.run()
